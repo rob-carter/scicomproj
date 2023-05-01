69ec488193fb7eae336cd4b28921fef6b56e3a8f
rob carter <robear95@icloud.com>
2023-04-30 23:31:07 -0600

    added image contraction

--- src/index_alt.js
+++ src/index_alt.js
@@ -1,4 +1,6 @@
-var canvas = document.getElementById("canvas"), width = 640 * 0.5, height = 300 * 0.5;
+function changeImage(imageName) {
+  document.getElementById("img").src = imageName;
+}
 
 const slider = document.getElementById('slider');
 
@@ -8,7 +10,16 @@ var vc = 0;
 
 const c = 299792458
 
+function lengthContraction(L, v) {
+  const gamma = 1 / Math.sqrt(1 - Math.pow(v/c, 2));
+  return L / gamma;
+}
+
 slider.addEventListener('input', (event) => {
   vc = event.target.value / 100;
   cdisLabel.textContent = `${vc}c`;
+  var newl = lengthContraction(800, vc*c) + 'px'
+  img.style.width = newl;
+  console.log(newl)
 });
+
