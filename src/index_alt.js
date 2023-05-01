662baa68f98e5be349bf558384141efaa206d2d2
rob carter <robear95@icloud.com>
2023-05-01 01:15:32 -0600

    added calculation to upper right

--- src/index_alt.js
+++ src/index_alt.js
@@ -4,7 +4,11 @@ function changeImage(imageName) {
 
 const slider = document.getElementById('slider');
 
-const cdisLabel = document.getElementById("cdis");
+const cdisLabel = document.getElementById('cdis');
+
+const calcs = document.getElementById('calcs');
+
+calcs.textContent = '800.00 = 800.00 * sqrt(1 - (0.0^2 / 299792458^2))'
 
 var vc = 0;
 
@@ -16,10 +20,11 @@ function lengthContraction(L, v) {
 }
 
 slider.addEventListener('input', (event) => {
-  vc = event.target.value / 100;
-  cdisLabel.textContent = `${vc}c`;
-  var newl = lengthContraction(800, vc*c) + 'px'
-  img.style.width = newl;
-  console.log(newl)
+  vc = event.target.value / 100
+  cdisLabel.textContent = `${vc}c`
+  var newl = lengthContraction(800, vc*c).toFixed(2)
+  img.style.width = newl + 'px'
+  // console.log(newl)
+  calcs.textContent = newl + ` = 800.00 * sqrt(1 - (${vc*c}^2 / ${c}^2))`
 });
 
